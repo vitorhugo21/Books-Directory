708ae01f2b3e0a5c2a86786f45f581a35b3ecf13
Vitor Hugo <vitortavares21@live.ua.pt>
2020-09-01 22:24:03 +0100

    Documentation done

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import dotenv from "dotenv";
+import cors from 'cors';
 import express from 'express';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import bookRoutes from './routes/book.route';
 
-dotenv.config();
+const swaggerDocumentPath = path.join(__dirname, 'data', 'swagger.yaml');
 
 const app = express();
-const port = process.env.SERVER_PORT ?
-  process.env.SERVER_PORT : 3000;
+const port = 8080;
 
+app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(YAML.load(swaggerDocumentPath)));
 app.use(bookRoutes);
 
 app.listen(port, () => {
